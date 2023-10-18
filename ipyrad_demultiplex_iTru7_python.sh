2ae92ed8bcbb032b3160b731bb9886ff3fd163b3
Zachary Hancock <zachbhancock@gmail.com>
2023-10-18 13:48:20 -0400

    updated

--- ipyrad_demultiplex_iTru7_python.sh
+++ ipyrad_demultiplex_iTru7_python.sh
@@ -1,22 +1,19 @@
 #1/bin/bash
-#SBATCH 
-time=10:00:00   # walltime
-nodes=1
-ntasks=15
-cpus-per-task=1 
-mem-per-cpu=10G   # memory per CPU
-mail-user=hancockz@umich.edu   # email address
-mail-type=BEGIN
-mail-type=END
-mail-type=FAIL
+#SBATCH --time=10:00:00   # walltimea
+#SBATCH --nodes=1
+#SBATCH --ntasks=15
+#SBATCH --cpus-per-task=1
+#SBATCH --mem-per-cpu=10G   # memory per CPU
+#SBATCH --mail-user=hancockz@umich.edu   # email address
+#SBATCH --mail-type=BEGIN
+#SBATCH --mail-type=END
+#SBATCH --mail-type=FAIL
 
 source activate ipyrad
 
 python run_ipyrad_demultiplex_itru7.py
 
 conda deactivate
-
-cd $SLURM_SUBMIT_DIR
 
 exit 0
 ~                
