6154ac8d7269f13a0a6dd04706ce9f28419ab7ba
Zachary Hancock <zachbhancock@gmail.com>
2023-10-18 14:09:48 -0400

    updated

--- ipyrad_demultiplex_iTru7_python.sh
+++ ipyrad_demultiplex_iTru7_python.sh
@@ -1,7 +1,6 @@
 #!/bin/bash
 #SBATCH --time=10:00:00   # walltimea
 #SBATCH --nodes=1
-#SBATCH --ntasks=15
 #SBATCH --cpus-per-task=1
 #SBATCH --mem-per-cpu=10G   # memory per CPU
 #SBATCH --mail-user=hancockz@umich.edu   # email address
