0c647d314b5bdeeaeb1c45edd9762493a2590a1d
Zachary Hancock <zachbhancock@gmail.com>
2023-10-18 13:58:38 -0400

    updated

--- run_ipyrad_demultiplex_iTru7.py
+++ run_ipyrad_demultiplex_iTru7.py
@@ -5,8 +5,8 @@ import ipyrad.analysis as ipa
 # demux outer i7s to plate1-16
 outer1 = ip.Assembly("demultiplexed_haustorius_reads")
 outer1.params.project_dir = "/scratch/bradburd_root/bradburd1/hancockz/haustorius_seqs"
-outer1.params.raw_fastq_path = "/scratch/bradburd_root/bradburd1/hancockz/haustorius_seqs/fastqs_9284-ZH/9284-ZH_*fastq.gz"
-outer1.params.barcodes_path = "/home/hancockz/shovel-bugs/Enzyme_barcodes.txt"
+outer1.params.raw_fastq_path = "/scratch/bradburd_root/bradburd1/hancockz/haustorius_seqs/fastqs_9284-ZH/9284-ZH-*fastq.gz"
+outer1.params.barcodes_path = "/home/hancockz/shovel-bugs/itru7_barcodes.txt"
 outer1.params.datatype = 'pairddrad'
 
 # important: set hackers params to demux on i7
