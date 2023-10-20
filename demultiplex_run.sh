c3469a577ea76811e0da0df2aca5278210ca6532
Zachary Hancock <zachbhancock@gmail.com>
2023-10-20 12:42:14 -0400

    added

---
+++ demultiplex_run.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+#SBATCH --time=10:00:00   # walltimea
+#SBATCH --nodes=1
+#SBATCH --ntasks=15
+#SBATCH --cpus-per-task=1
+#SBATCH --mem-per-cpu=10G   # memory per CPU
+#SBATCH --mail-user=hancockz@umich.edu   # email address
+#SBATCH --mail-type=BEGIN,END,FAIL
+
+source activate ipyrad
+
+ipyrad -p params-newdata.txt -s 1
+
+conda deactivate
+
+exit 0
+~      
